82d7d1a7f128d0045427dddef45998256957e072
lim2222 <johnlky222@gmail.com>
2026-06-07 19:08:06 +0800

    Add touchscreen tatacon (taiko drum) control

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -983,6 +983,7 @@ LOCAL_SRC_FILES := \
   $(SRC)/UI/TiltAnalogSettingsScreen.cpp \
   $(SRC)/UI/TouchControlLayoutScreen.cpp \
   $(SRC)/UI/TouchControlVisibilityScreen.cpp \
+  $(SRC)/UI/TataconControl.cpp \
   $(SRC)/UI/BaseScreens.cpp \
   $(SRC)/UI/Background.cpp \
   $(SRC)/UI/CwCheatScreen.cpp \
