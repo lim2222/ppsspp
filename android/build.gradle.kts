66c73e736faf37dbc019c7b713eaef0bbddc4ec2
紀全 <your.email@example.com>
2026-09-03 22:04:38 +0800

    Downgrade AGP to 9.2.1 and SDK to 35 for compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -105,7 +105,7 @@ android {
 		}
 	}
 
-	compileSdk = 37
+	compileSdk = 35
 	ndkVersion = "29.0.14206865"
 
 	compileOptions {
@@ -131,7 +131,7 @@ android {
 		file("versioncode.txt").writeText(gitVersionCode.toString())
 
 		minSdk = 21
-		targetSdk = 37
+		targetSdk = 35
 		if (project.hasProperty("ANDROID_VERSION_CODE") && project.hasProperty("ANDROID_VERSION_NAME")) {
 			versionCode = (project.property("ANDROID_VERSION_CODE") as String).toInt()
 			versionName = project.property("ANDROID_VERSION_NAME") as String
